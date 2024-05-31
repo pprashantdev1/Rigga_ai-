946bb01d83aab2be3325f13f55a2cb0e6ecee50f
prashant sahu <prashantmanisahukhaga@gmail.com>
2024-05-31 16:55:31 +0530

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    
+$('#menu').click(function(){
+    $('#show').toggleClass('show');
+    $('#menu').toggleClass('fa-bars fa-x');
+    $('#navbar').toggleClass('show-color');
+    $('#navbar').toggleClass('br')
+})
+})
